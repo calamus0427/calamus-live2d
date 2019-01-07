defdb2a60adc4777c85da80b500d0a64d3ed4cb6
calamus0427 <1211994507@qq.com>
2019-01-07 17:05:54 +0800

    new my

--- demo/assets/waifu-tips.js
+++ demo/assets/waifu-tips.js
@@ -1,8 +1,8 @@
-String.prototype.render = function (context) {
+String.prototype.render = function(context) {
     var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
 
-    return this.replace(tokenReg, function (word, slash1, token, slash2) {
-        if (slash1 || slash2) {  
+    return this.replace(tokenReg, function(word, slash1, token, slash2) {
+        if (slash1 || slash2) {
             return word.replace('\\', '');
         }
 
@@ -18,70 +18,75 @@ String.prototype.render = function (context) {
         return currentObject;
     });
 };
+console.log("%c【calamus】\n%ckiz.calamus@gmail.com", "color:#E1244E;font-size:16px;text-shadow:3px 1px 2px #e1244e9e;", "color:#9e9e9e;font-size:14px")
+console.log("%c ", "background: url(http://p79mwfmry.bkt.clouddn.com/logo50.jpg) no-repeat center;padding-left:80px;padding-bottom: 80px;border-radius:50%;")
+console.log("%c你可以选择爱我或者不爱我\n而我\n只能选择爱你或者更爱你\n", "color:#e1244e;font-size:14px;text-shadow:3px 1px 2px #e1244e9e;")
+console.log("%c ", "background: url(http://p3i10hjs7.bkt.clouddn.com/console.jpeg) no-repeat center;padding-left:640px;padding-bottom: 242px;")
 
 var re = /x/;
-console.log(re);
 re.toString = function() {
     showMessage('哈哈，你打开了控制台，是想要看看我的秘密吗？', 5000, true);
+    console.log("%c【calamus】\n%ckiz.calamus@gmail.com", "color:#E1244E;font-size:16px;text-shadow:3px 1px 2px #e1244e9e;", "color:#9e9e9e;font-size:14px")
+    console.log("%c ", "background: url(http://p79mwfmry.bkt.clouddn.com/logo50.jpg) no-repeat center;padding-left:80px;padding-bottom: 80px;border-radius:50%;")
+    console.log("%c你可以选择爱我或者不爱我\n而我\n只能选择爱你或者更爱你\n", "color:#e1244e;font-size:14px;text-shadow:3px 1px 2px #e1244e9e;")
+    console.log("%c ", "background: url(http://p3i10hjs7.bkt.clouddn.com/console.jpeg) no-repeat center;padding-left:640px;padding-bottom: 242px;")
     return '';
 };
 
-$(document).on('copy', function (){
+$(document).on('copy', function() {
     showMessage('你都复制了些什么呀，转载要记得加上出处哦', 5000, true);
 });
 
 $('.waifu-tool .cl-home')
-    .click(function () {
-    //window.location = 'https://www.fghrsh.net/';
-    window.location = window.location.protocol+'//'+window.location.hostname+'/'
-});
+    .click(function() {
+        window.location = window.location.protocol + '//' + window.location.hostname + '/'
+    });
 
 $('.waifu-tool .cl-nv')
-    .click(function () {
-    loadOtherModel();
-});
+    .click(function() {
+        loadOtherModel();
+    });
 
 $('.waifu-tool .cl-liaotianduihua')
-    .click(function () {
-    showHitokoto();
-});
+    .click(function() {
+        showHitokoto();
+    });
 
 $('.waifu-tool .cl-nvzhuangqunzi-1')
-    .click(function () {
-    loadRandModel();
-});
+    .click(function() {
+        loadRandModel();
+    });
 
 $('.waifu-tool .cl-github1')
-    .click(function () {
-window.open('https://github.com/calamus0427');
-});
+    .click(function() {
+        window.open('https://github.com/calamus0427');
+    });
 
 $('.waifu-tool .cl-weixin')
-    .click(function () {
-showMessage('关注人家的微信公众号<span style="font-size:16px;color:orange"> kiz_alamus </span>好喵，求你了φ(>ω<*) ',
-2000, true);
+    .click(function() {
+        showMessage('关注人家的微信公众号<span style="font-size:16px;color:orange"> kiz_alamus </span>好喵，求你了φ(>ω<*) ',
+            2000, true);
     });
 
 $('.waifu-tool .cl-yincang')
-    .click(function () {
-    sessionStorage.setItem('waifu-dsiplay', 'none');
-    showMessage('愿你有一天能与重要的人重逢', 1300, true);
-    window.setTimeout(function() {$('.waifu').hide();}, 1300);
-});
+    .click(function() {
+        sessionStorage.setItem('waifu-dsiplay', 'none');
+        showMessage('愿你有一天能与重要的人重逢', 1300, true);
+        window.setTimeout(function() { $('.waifu').hide(); }, 1300);
+    });
 
 $('.waifu-tool .cl-paizhao')
-    .click(function () {
-    showMessage('照好了嘛，是不是很可爱喵？', 5000, true);
-    window.Live2D.captureName = 'Pio.png';
-    window.Live2D.captureFrame = true;
-});
+    .click(function() {
+        showMessage('照好了嘛，是不是很可爱喵？', 5000, true);
+        window.Live2D.captureName = 'Pio.png';
+        window.Live2D.captureFrame = true;
+    });
 
-(function (){
+(function() {
     var text;
-    //var SiteIndexUrl = 'https://www.fghrsh.net/';  // 手动指定主页
-    var SiteIndexUrl = window.location.protocol+'//'+window.location.hostname+'/';  // 自动获取主页
-    
-    if (window.location.href == SiteIndexUrl) {      // 如果是主页
+    var SiteIndexUrl = window.location.protocol + '//' + window.location.hostname + '/'; // 自动获取主页
+
+    if (window.location.href == SiteIndexUrl) { // 如果是主页
         var now = (new Date()).getHours();
         if (now > 23 || now <= 5) {
             text = '嗨你麻痹，滚去睡';
@@ -103,23 +108,35 @@ $('.waifu-tool .cl-paizhao')
             text = '嗨~ 快来逗我玩吧！';
         }
     } else {
-        if(document.referrer !== ''){
+        if (document.referrer !== '') {
             var referrer = document.createElement('a');
             referrer.href = document.referrer;
             var domain = referrer.hostname.split('.')[1];
             if (window.location.hostname == referrer.hostname) {
-                text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+                text = '欢迎阅读<span style="color:orange;">『' + document
+                    .title
+                    .split(' - ')[0] + '』</span>';
             } else if (domain == 'baidu') {
-                text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
+                text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:orange;">' + referrer
+                    .search
+                    .split('&wd=')[1]
+                    .split('&')[0] + '</span> 找到的我吗？';
             } else if (domain == 'so') {
-                text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&q=')[1].split('&')[0] + '</span> 找到的我吗？';
+                text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:orange;">' + referrer
+                    .search
+                    .split('&q=')[1]
+                    .split('&')[0] + '</span> 找到的我吗？';
             } else if (domain == 'google') {
-                text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+                text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:orange;">『' + document
+                    .title
+                    .split(' - ')[0] + '』</span>';
             } else {
-                text = 'Hello! 来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友';
+                text = 'Hello! 来自 <span style="color:orange">' + referrer.hostname + '</span> 的朋友';
             }
         } else {
-            text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+            text = '欢迎阅读<span style="color:orange">『' + document
+                .title
+                .split(' - ')[0] + '』</span>';
         }
     }
     showMessage(text, 6000);
@@ -131,8 +148,11 @@ var getActed = false;
 window.hitokotoTimer = 0;
 var hitokotoInterval = false;
 
-$(document).mousemove(function(e){getActed = true;}).keydown(function(){getActed = true;});
-setInterval(function() { if (!getActed) ifActed(); else elseActed(); }, 1000);
+$(document).mousemove(function(e) { getActed = true; }).keydown(function() { getActed = true; });
+setInterval(function() {
+    if (!getActed) ifActed();
+    else elseActed();
+}, 1000);
 
 function ifActed() {
     if (!hitokotoInterval) {
@@ -146,19 +166,24 @@ function elseActed() {
     window.clearInterval(hitokotoTimer);
 }
 
-function showHitokoto(){
-    $.getJSON('//api.fghrsh.net/hitokoto/rand/?encode=jsc&uid=3335',function(result){
-        var text = '这句一言出处是 <span style="color:#0099cc;">『{source}』</span>，是 <span style="color:#0099cc;">FGHRSH</span> 在 {date} 收藏的！';
-        text = text.render({source: result.source, date: result.date});
+function showHitokoto() {
+    $.getJSON('//v1.hitokoto.cn/?encode=json', function(result) {
+        var text = '';
+        if(result.source){
+            text = '{hitokoto} <span style="color:orange;">----{source}</span>'
+        }else{
+            text= '{hitokoto}'
+        }
+        text = text.render({ hitokoto:result.hitokoto,source: result.source, creator:result.creator,date: result.created_at });
         showMessage(result.hitokoto, 5000);
-        window.setTimeout(function() {showMessage(text, 3000);}, 5000);
+        // window.setTimeout(function() { showMessage(text, 3000); }, 5000);
     });
 }
 
-function showMessage(text, timeout, flag){
-    if(flag || sessionStorage.getItem('waifu-text') === '' || sessionStorage.getItem('waifu-text') === null){
-        if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
-        if(flag) sessionStorage.setItem('waifu-text', text);
+function showMessage(text, timeout, flag) {
+    if (flag || sessionStorage.getItem('waifu-text') === '' || sessionStorage.getItem('waifu-text') === null) {
+        if (Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1) - 1];
+        if (flag) sessionStorage.setItem('waifu-text', text);
         $('.waifu-tips').stop();
         $('.waifu-tips').html(text).fadeTo(200, 1);
         if (timeout === undefined) timeout = 5000;
@@ -166,61 +191,61 @@ function showMessage(text, timeout, flag){
     }
 }
 
-function hideMessage(timeout){
-    $('.waifu-tips').stop().css('opacity',1);
+function hideMessage(timeout) {
+    $('.waifu-tips').stop().css('opacity', 1);
     if (timeout === undefined) timeout = 5000;
-    window.setTimeout(function() {sessionStorage.removeItem('waifu-text')}, timeout);
+    window.setTimeout(function() { sessionStorage.removeItem('waifu-text') }, timeout);
     $('.waifu-tips').delay(timeout).fadeTo(200, 0);
 }
 
-function initModel(waifuPath){
-    
+function initModel(waifuPath) {
+
     if (waifuPath === undefined) waifuPath = '';
     var modelId = localStorage.getItem('modelId');
     var modelTexturesId = localStorage.getItem('modelTexturesId');
-    
+
     if (modelId == null) {
-        
+
         /* 首次访问加载 指定模型 的 指定材质 */
-        
-        var modelId = 6;            // 模型 ID
-        var modelTexturesId = 4    // 材质 ID
-        
-    } 
-    
+
+        var modelId = 6; // 模型 ID
+        var modelTexturesId = 8 // 材质 ID
+
+    }
+
     loadModel(modelId, modelTexturesId);
-	
-	$.ajax({
+
+    $.ajax({
         cache: true,
-        url: waifuPath+'waifu-tips.json',
+        url: waifuPath + 'waifu-tips.json',
         dataType: "json",
-        success: function (result){
-            $.each(result.mouseover, function (index, tips){
-                $(document).on("mouseover", tips.selector, function (){
+        success: function(result) {
+            $.each(result.mouseover, function(index, tips) {
+                $(document).on("mouseover", tips.selector, function() {
                     var text = tips.text;
-                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
-                    text = text.render({text: $(this).text()});
+                    if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+                    text = text.render({ text: $(this).text() });
                     showMessage(text, 3000);
                 });
             });
-            $.each(result.click, function (index, tips){
-                $(document).on("click", tips.selector, function (){
+            $.each(result.click, function(index, tips) {
+                $(document).on("click", tips.selector, function() {
                     var text = tips.text;
-                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
-                    text = text.render({text: $(this).text()});
+                    if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+                    text = text.render({ text: $(this).text() });
                     showMessage(text, 3000, true);
                 });
             });
-            $.each(result.seasons, function (index, tips){
+            $.each(result.seasons, function(index, tips) {
                 var now = new Date();
                 var after = tips.date.split('-')[0];
                 var before = tips.date.split('-')[1] || after;
-                
-                if((after.split('/')[0] <= now.getMonth()+1 && now.getMonth()+1 <= before.split('/')[0]) && 
-                   (after.split('/')[1] <= now.getDate() && now.getDate() <= before.split('/')[1])){
+
+                if ((after.split('/')[0] <= now.getMonth() + 1 && now.getMonth() + 1 <= before.split('/')[0]) &&
+                    (after.split('/')[1] <= now.getDate() && now.getDate() <= before.split('/')[1])) {
                     var text = tips.text;
-                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
-                    text = text.render({year: now.getFullYear()});
+                    if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+                    text = text.render({ year: now.getFullYear() });
                     showMessage(text, 6000, true);
                 }
             });
@@ -228,24 +253,24 @@ function initModel(waifuPath){
     });
 }
 
-function loadModel(modelId, modelTexturesId){
+function loadModel(modelId, modelTexturesId) {
     localStorage.setItem('modelId', modelId);
     if (modelTexturesId === undefined) modelTexturesId = 0;
     localStorage.setItem('modelTexturesId', modelTexturesId);
-    loadlive2d('live2d', '//api.fghrsh.net/live2d/get/?id='+modelId+'-'+modelTexturesId, console.log('live2d','模型 '+modelId+'-'+modelTexturesId+' 加载完成'));
+    loadlive2d('live2d', '//www.calamus.xyz/live2d/get/?id=' + modelId + '-' + modelTexturesId, console.log('看板娘', '' + modelId + '-' + modelTexturesId + ' 为你提供特殊服务哦'));
 }
 
-function loadRandModel(){
+function loadRandModel() {
     var modelId = localStorage.getItem('modelId');
     var modelTexturesId = localStorage.getItem('modelTexturesId');
-    
-    var modelTexturesRandMode = 'rand';     // 可选 'rand'(随机), 'switch'(顺序)
-    
+
+    var modelTexturesRandMode = 'rand'; // 可选 'rand'(随机), 'switch'(顺序)
+
     $.ajax({
         cache: false,
-        url: '//api.fghrsh.net/live2d/'+modelTexturesRandMode+'_textures/?id='+modelId+'-'+modelTexturesId,
+        url: '//www.calamus.xyz/live2d/' + modelTexturesRandMode + '_textures/?id=' + modelId + '-' + modelTexturesId,
         dataType: "json",
-        success: function (result){
+        success: function(result) {
             if (result.textures['id'] == 1 && (modelTexturesId == 1 || modelTexturesId == 0)) {
                 showMessage('人家还没有其他衣服呢', 3000, true);
             } else {
@@ -256,16 +281,16 @@ function loadRandModel(){
     });
 }
 
-function loadOtherModel(){
+function loadOtherModel() {
     var modelId = localStorage.getItem('modelId');
-    
-    var modelTexturesRandMode = 'switch';     // 可选 'rand'(随机), 'switch'(顺序)
-    
+
+    var modelTexturesRandMode = 'switch'; // 可选 'rand'(随机), 'switch'(顺序)
+
     $.ajax({
         cache: false,
-        url: '//api.fghrsh.net/live2d/'+modelTexturesRandMode+'/?id='+modelId,
+        url: '//www.calamus.xyz/live2d/' + modelTexturesRandMode + '/?id=' + modelId,
         dataType: "json",
-        success: function (result){
+        success: function(result) {
             loadModel(result.model['id']);
             showMessage(result.model['message'], 3000, true);
         }
